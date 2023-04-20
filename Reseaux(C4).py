7a3a47932ab6ae82c31b9e31a4cbaa0ac11e4a01
Yann EDIKEU <yann.edikeu@etu.u-paris.fr>
2023-04-20 17:08:27 +0200

    ajt de Réseaux(C4) mais encore des erreurs a corriger

--- Reseaux(C4).py
+++ Reseaux(C4).py
@@ -1,92 +1,113 @@
-def F(k, x):
-    # k est une clé de 16 bits, représentée sous forme d'un entier en base 10
-    # x est un bloc de 16 bits, représenté sous forme d'un entier en base 10
-    # Les valeurs de k et x doivent être comprises entre 0 et 65535 inclusivement
-    
-    # Conversion de k en une liste de 16 bits
-    key = [(k >> i) & 1 for i in range(16)]
-    
-    # Conversion de x en une liste de 16 bits
-    block = [(x >> i) & 1 for i in range(16)]
-    
-    # Application de la substitution
-    s_box = [0xE, 0x4, 0xD, 0x1, 0x2, 0xF, 0xB, 0x8, 0x3, 0xA, 0x6, 0xC, 0x5, 0x9, 0x0, 0x7]
+def getBits(nb):
+    if isinstance(nb, bytes):
+        nb = int.from_bytes(nb, byteorder='little')
+    res = [False]*16
     for i in range(16):
-        block[i] = s_box[block[i]]
-    
-    # Conversion de la liste de bits en un entier
-    block = sum([block[i] * 2**(15-i) for i in range(16)])
-    
-    # Application de la permutation
-    perm = [1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15, 4, 8, 12, 16]
-    block_bits = [(block >> (16-i)) & 1 for i in range(1, 17)]
-    perm_bits = [block_bits[perm[i]-1] for i in range(16)]
-    perm_block = sum([perm_bits[i] * 2**(15-i) for i in range(16)])
-    
-    return perm_block
+        if nb & (1 << (15 - i)):
+            res[i] = True
+    return res
 
-def encrypt_ctr(key, nonce, plaintext):
-    # key est une clé de 16 bits, représentée sous forme d'un entier en base 10
-    # nonce est un nonce de 16 bits, représenté sous forme d'un entier en base 10
-    # plaintext est un texte en clair, représenté sous forme d'une chaîne de caractères
-    # La longueur de plaintext doit être un multiple de 2
-    
-    # Séparation de plaintext en blocs de 16 bits
-    blocks = [int(plaintext[i:i+2], 16) for i in range(0, len(plaintext), 2)]
-    
-    # Calcul de la clé pour chaque tour
-    keys = [key, ((key << 4) & 0xFFFF) | (key >> 12), ((key << 8) & 0xFFFF) | (key >> 8), ((key << 12) & 0xFFFF) | (key >> 4)]
-    
-    # Initialisation du compteur
-    counter = nonce
-    
-    # Chiffrement des blocs en utilisant la fonction F en mode compteur
-    ciphertext = ''
-    for block in blocks:
-        # Calcul du bloc chiffré
-        encrypted_block = F(keys[counter % 4], counter) ^ block
-        
-        # Conversion du bloc chiffré en une chaîne de caractères hexadécimaux de longueur 4
-        ciphertext_block = hex(encrypted_block)[2:].zfill(4)
-        
-        # Ajout du bloc chiffré au texte chiffré final
-        ciphertext += ciphertext_block
-        
-        # Incrémentation du compteur
+
+
+def bitsToNb(bits):
+    res = 0
+    for i in range(16):
+        if bits[i]:
+            res += 2**(15 - i)
+    return res
+
+def xorBit(x1, x2):
+    return (x1 or x2) and not (x1 and x2)
+
+def xor(n1, n2):
+    b1 = getBits(n1)
+    b2 = getBits(n2)
+    bits = [False]*16
+    for i in range(16):
+        bits[i] = xorBit(b1[i], b2[i])
+    return bitsToNb(bits)
+
+def cutInFour(tab):
+    part1 = tab[0:4]
+    part2 = tab[4:8]
+    part3 = tab[8:12]
+    part4 = tab[12:]
+    return [part1, part2, part3, part4]
+
+def concat_parts(tab):
+    # Note : intended ESPECIALLY to merge 4 4-bits lists into a 16-bit one !!!
+    return tab[0] + tab[1] + tab[2] + tab[3]
+
+def C4_sub(n):
+    tab = [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7]
+    return tab[n]
+
+def C4_Perm(tab):
+    res = tab
+    perm = [1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15, 4, 8, 12, 16]
+    for i in range(16):
+        res[i] = tab[perm[i]]
+    return res
+
+def getC4Keys(k):
+    tab = getBits(k)  # Get bits of the 16-bits key k
+    part1 = tab[0:4]
+    part2 = tab[4:8]
+    part3 = tab[8:12]
+    part4 = tab[12:]
+    k1 = tab
+    k2 = part4 + part1 + part2 + part3
+    k3 = part3 + part4 + part1 + part2
+    k4 = part2 + part3 + part4 + part1
+    return [k1, k2, k3, k4]
+
+def F_helper(k, x):
+    keys = getC4Keys(k)
+    z = x
+    for i in range(4):
+        z = xor(z, keys[i])
+        z_parts = cutInFour(getBits(z))
+        sub_parts = [C4_sub(part) for part in z_parts]
+        z = concat_parts(sub_parts)
+        z = C4_Perm(z)
+        z = bitsToNb(z)
+    return z
+
+def F(k):
+    return lambda x: F_helper(k, x)
+
+
+import struct
+
+def encrypt(key, nonce, plaintext):
+    counter = 0
+    ciphertext = b""
+    Fk = F(key)
+    plaintext = bytes.fromhex(plaintext)  # convert the hex string to bytes
+    while len(plaintext) > 0:
+        # generate pseudorandom number
+        keystream = Fk((struct.pack("<H", nonce) + struct.pack("<H", counter)))
+        # XOR with plaintext to produce ciphertext
+        chunk = plaintext[:2]
+        plaintext = plaintext[2:]
+        ciphertext += bytes([chunk[i] ^ keystream[i] for i in range(2)])
+        # increment counter
         counter += 1
-    
     return ciphertext
 
-def decrypt_ctr(key, nonce, ciphertext):
-    # key est une clé de 16 bits, représentée sous forme d'un entier en base 10
-    # nonce est un nonce de 16 bits, représenté sous forme d'un entier en base 10
-    # ciphertext est un texte chiffré, représenté sous forme d'une chaîne de caractères
-    # La longueur de ciphertext doit être un multiple de 4
-    
-    # Séparation de ciphertext en blocs de 16 bits
-    blocks = [int(ciphertext[i:i+4], 16) for i in range(0, len(ciphertext), 4)]
-    
-    # Calcul de la clé pour chaque tour
-    keys = [key, ((key << 4) & 0xFFFF) | (key >> 12), ((key << 8) & 0xFFFF) | (key >> 8), ((key << 12) & 0xFFFF) | (key >> 4)]
-    
-    # Initialisation du compteur
-    counter = nonce
-    
-    # Déchiffrement des blocs en utilisant la fonction F en mode compteur
-    plaintext = ''
-    for block in blocks:
-        # Calcul du bloc déchiffré
-        decrypted_block = F(keys[counter % 4], counter) ^ block
-        
-        # Conversion du bloc déchiffré en une chaîne de caractères hexadécimaux de longueur 4
-        plaintext_block = hex(decrypted_block)[2:].zfill(4)
-        
-        # Ajout du bloc déchiffré au texte déchiffré final
-        plaintext += plaintext_block
-        
-        # Incrémentation du compteur pour le bloc suivant
-        counter += 1
-    
-    return plaintext
 
-    
+def decrypt(key, nonce, ciphertext):
+    return encrypt(key, nonce, ciphertext) # decryption is the same as encryption in CTR mode
+
+
+#--------------------------------------------   TESTS   ------------------------------------------------------------
+key = 0x3A5C
+nonce = 0x7B24
+plaintext = '6BC1BEE22E409F96E93D7E117393172A'
+
+ciphertext = encrypt(key, nonce, plaintext)
+print(ciphertext)
+
+decrypted_plaintext = decrypt(key, nonce, ciphertext)
+print(decrypted_plaintext)
+
